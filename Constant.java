1eb0ceaafea5c89f2e5b8156b13177faa6e1522b
tenSunFree <a0985092384@gmail.com>
2016-10-29 22:27:01 +0800

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,7 @@
+package net.macdidi.face_detection;
+
+
+public class Constant {
+    public static final String KEY = "eb0440c2c4dae1cdc492c97d29ebe02b";
+    public static final String SECRET = "HHuvC_hnVORk0RdKA-A1JSDmOxg67VbB";
+}
